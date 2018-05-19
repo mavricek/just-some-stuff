3dba85b1a2e73cb4b4f09c1e0f6c385b82ce48fa
Matej <mmavricek@burning-glass.com>
2018-05-19 12:23:24 -0400

    igp3

---
+++ learning_python/illustrated_guide_to_python3/hello.py
@@ -0,0 +1,15 @@
+#print("hello world")
+#print("Hello there")
+#print("I am", 32, "years old")
+
+#name = input('Enter your name, please: ')
+#print(name)
+
+#value = input('Enter a number: ')
+#other = input('Enter another: ')
+
+#print(value + other) # gives 34
+#print(int(value) + int(other)) # gives 7
+
+age = input('Please input your age: ')
+print('You will be', int(age) + 1, 'years old next year')
